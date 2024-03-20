cbedf8364a91f53c4dbfc5dad9085c45faa353b1
PauloUbirajara <ubirajara3@hotmail.com>
2024-03-19 23:18:45 -0300

    test: ensure transaction is created if valid data is provided

--- models/transaction_test.go
+++ models/transaction_test.go
@@ -7,7 +7,7 @@ import (
 	models "financial-parsing/models"
 )
 
-func TestTransaction_ShouldBelongToAValidWallet(t *testing.T) {
+func TestTransaction_ShouldFailIfWalletIsNull(t *testing.T) {
 	transaction, err := models.NewTransaction(nil, "Valid Name", "Valid Description", 123.45, time.Now())
 
 	if err == nil {
@@ -16,5 +16,17 @@ func TestTransaction_ShouldBelongToAValidWallet(t *testing.T) {
 
 	if transaction != nil {
 		t.Errorf("Created invalid transaction with null wallet")
+	}
+}
+
+func TestTransaction_ShouldPassIfValidData(t *testing.T) {
+	transaction, err := models.NewTransaction(&models.Wallet{}, "Valid Name", "Valid Description", 123.45, time.Now())
+
+	if err != nil {
+		t.Errorf("Threw error when creating valid transaction")
+	}
+
+	if transaction == nil {
+		t.Errorf("Created null transaction with valid values")
 	}
 }
