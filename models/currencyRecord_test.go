10325de21ddadad056ab029f92dbd51f5843eb64
PauloUbirajara <ubirajara3@hotmail.com>
2024-03-20 13:44:20 -0300

    test: ensure currency record is created if valid data is provided

---
+++ models/currencyRecord_test.go
@@ -0,0 +1,32 @@
+package models_test
+
+import (
+	"testing"
+	"time"
+
+	models "financial-parsing/models"
+)
+
+func TestCurrencyRecord_ShouldFailIfCurrencyIsNull(t *testing.T) {
+	currencyRecord, err := models.NewCurrencyRecord(nil, "Valid Name", 123.45, time.Now())
+
+	if err == nil {
+		t.Errorf("Did not throw error when creating invalid currencyRecord")
+	}
+
+	if currencyRecord != nil {
+		t.Errorf("Created invalid currencyRecord with null currency")
+	}
+}
+
+func TestCurrencyRecord_ShouldPassIfValidData(t *testing.T) {
+	currencyRecord, err := models.NewCurrencyRecord(&models.Currency{}, "Valid Name", 123.45, time.Now())
+
+	if err != nil {
+		t.Errorf("Threw error when creating valid currency record")
+	}
+
+	if currencyRecord == nil {
+		t.Errorf("Created null currency record with valid values")
+	}
+}
