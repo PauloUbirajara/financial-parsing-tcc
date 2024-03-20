7518c70e7917c428cbc29365cd7638947b8ce0e1
PauloUbirajara <ubirajara3@hotmail.com>
2024-03-20 16:35:33 -0300

    test: ensure currency contains data from parameters

---
+++ domain/models/currency_test.go
@@ -0,0 +1,31 @@
+package models_test
+
+import (
+	"testing"
+
+	models "financial-parsing/domain/models"
+)
+
+func validCurrency() (*models.Currency, error) {
+	return models.NewCurrency("Valid Name", "VALIDREPRESENTATION")
+}
+
+func TestCurrency_ShouldContainData(t *testing.T) {
+	currency, _ := validCurrency()
+
+	if currency.CreatedAt.IsZero() { t.Errorf("Valid currency does did not include createdAt when created") }
+	if currency.Name == "" { t.Errorf("Valid currency does did not include name when created") }
+	if currency.Representation == "" { t.Errorf("Valid currency does did not include representation when created") }
+}
+
+func TestCurrency_ShouldPassIfValidData(t *testing.T) {
+	currency, err := validCurrency()
+
+	if err != nil {
+		t.Errorf("Threw error when creating valid currency record")
+	}
+
+	if currency == nil {
+		t.Errorf("Created null currency record with valid values")
+	}
+}
