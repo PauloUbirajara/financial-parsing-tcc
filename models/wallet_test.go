50494405e5b10d2f1052a6b0bf92039000b20700
PauloUbirajara <ubirajara3@hotmail.com>
2024-03-19 23:24:06 -0300

    test: ensure wallet is created with non-null currency

---
+++ models/wallet_test.go
@@ -0,0 +1,31 @@
+package models_test
+
+import (
+	"testing"
+
+	models "financial-parsing/models"
+)
+
+func TestWallet_ShouldFailIfCurrencyIsNull(t *testing.T) {
+	wallet, err := models.NewWallet(nil, "Valid Name", "Valid Description")
+
+	if err == nil {
+		t.Errorf("Did not throw error when creating invalid wallet")
+	}
+
+	if wallet != nil {
+		t.Errorf("Created invalid wallet with null currency")
+	}
+}
+
+func TestWallet_ShouldPassIfValidData(t *testing.T) {
+	wallet, err := models.NewWallet(&models.Currency{}, "Valid Name", "Valid Description")
+
+	if err != nil {
+		t.Errorf("Threw error when creating valid wallet")
+	}
+
+	if wallet == nil {
+		t.Errorf("Created null wallet with valid values")
+	}
+}
