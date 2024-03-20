ea7d9a87e97b05d23be56c4193d24ccb525455a1
PauloUbirajara <ubirajara3@hotmail.com>
2024-03-20 13:50:05 -0300

    refactor: move test transaction creation to helper functions

--- models/transaction_test.go
+++ models/transaction_test.go
@@ -7,8 +7,16 @@ import (
 	models "financial-parsing/models"
 )
 
+func transactionWithMissingWallet() (*models.Transaction, error) {
+	return models.NewTransaction(nil, "Valid Name", "Valid Description", 123.45, time.Now())
+}
+
+func validTransaction() (*models.Transaction, error) {
+	return models.NewTransaction(&models.Wallet{}, "Valid Name", "Valid Description", 123.45, time.Now())
+}
+
 func TestTransaction_ShouldFailIfWalletIsNull(t *testing.T) {
-	transaction, err := models.NewTransaction(nil, "Valid Name", "Valid Description", 123.45, time.Now())
+	transaction, err := transactionWithMissingWallet()
 
 	if err == nil {
 		t.Errorf("Did not throw error when creating invalid transaction")
@@ -20,7 +28,7 @@ func TestTransaction_ShouldFailIfWalletIsNull(t *testing.T) {
 }
 
 func TestTransaction_ShouldPassIfValidData(t *testing.T) {
-	transaction, err := models.NewTransaction(&models.Wallet{}, "Valid Name", "Valid Description", 123.45, time.Now())
+	transaction, err := validTransaction()
 
 	if err != nil {
 		t.Errorf("Threw error when creating valid transaction")
