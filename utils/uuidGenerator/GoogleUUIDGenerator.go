40d9662738293161e73dfba1bac7720e374cdd6f
PauloUbirajara <ubirajara3@hotmail.com>
2024-03-20 16:31:20 -0300

    feature: ensure uuid generator returns uuid as string

---
+++ utils/uuidGenerator/GoogleUUIDGenerator.go
@@ -0,0 +1,12 @@
+package uuidgenerator
+
+import (
+	"github.com/google/uuid"
+)
+
+type GoogleUUIDGenerator struct{}
+
+func (g *GoogleUUIDGenerator) Generate() string {
+	uuid := uuid.Must(uuid.NewV7())
+	return uuid.String()
+}
