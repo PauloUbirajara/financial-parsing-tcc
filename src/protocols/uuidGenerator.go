59e3a2f8eef5f43fcf79e28d770786b6311e1550
PauloUbirajara <ubirajara3@hotmail.com>
2024-03-25 18:20:18 -0300

    feature: add validation method to uuidgenerator

--- src/protocols/uuidGenerator.go
+++ src/protocols/uuidGenerator.go
@@ -2,4 +2,5 @@ package protocols
 
 type UUIDGenerator interface {
 	Generate() string
+	IsValidUUID(id string) error
 }
--- src/utils/uuidGenerator/GoogleUUIDGenerator.go
+++ src/utils/uuidGenerator/GoogleUUIDGenerator.go
@@ -10,3 +10,7 @@ func (g GoogleUUIDGenerator) Generate() string {
 	uuid := uuid.Must(uuid.NewV7())
 	return uuid.String()
 }
+
+func (g GoogleUUIDGenerator) IsValidUUID(id string) error  {
+	return uuid.Validate(id)
+}
