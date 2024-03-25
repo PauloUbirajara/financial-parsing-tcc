986d5c122d28ca927509d822cbcf2deeb705f305
PauloUbirajara <ubirajara3@hotmail.com>
2024-03-25 15:33:53 -0300

    test: refactor test case name

--- src/utils/uuidGenerator/GoogleUUIDGenerator_test.go
+++ src/utils/uuidGenerator/GoogleUUIDGenerator_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	protocols "financial-parsing/src/protocols"
+
 	uuidGenerator "financial-parsing/src/utils/uuidGenerator"
 )
 
@@ -19,7 +20,7 @@ func NewUUIDGeneratorTestData() *UUIDGeneratorTestData {
 	return &testData
 }
 
-func TestGoogleUUIDGenerator_ShouldGenerateUUID(t *testing.T) {
+func TestGoogleUUIDGeneratorShouldGenerateUUID(t *testing.T) {
 	testData := NewUUIDGeneratorTestData()
 	uuid := testData.sut.Generate()
 
