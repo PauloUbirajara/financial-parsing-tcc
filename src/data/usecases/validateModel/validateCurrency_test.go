61c328a1c2c9a2f51d6fd3628bff0bec50264ae1
PauloUbirajara <ubirajara3@hotmail.com>
2024-03-25 23:49:04 -0300

    test: ensure ValidateCurrency fails on empty name

---
+++ src/data/usecases/validateModel/validateCurrency_test.go
@@ -0,0 +1,63 @@
+package validatemodel_test
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	models "financial-parsing/src/domain/models"
+	usecases "financial-parsing/src/domain/usecases"
+
+	validatemodel "financial-parsing/src/data/usecases/validateModel"
+
+	"gorm.io/gorm"
+)
+
+type ValidateCurrencyTestData struct {
+	sut      usecases.ValidateModel
+	currency models.Currency
+}
+
+type UUIDGeneratorStub struct{}
+
+func (u UUIDGeneratorStub) Generate() string {
+	return ""
+}
+
+func (u UUIDGeneratorStub) IsValidUUID(id string) error {
+	if id != "valid id" {
+		return errors.New("invalid id")
+	}
+	return nil
+}
+
+func newValidCurrency() models.Currency {
+	return models.Currency{
+		ID:             "valid id",
+		CreatedAt:      time.Now(),
+		UpdatedAt:      time.Now(),
+		DeletedAt:      gorm.DeletedAt{},
+		Name:           "valid name",
+		Representation: "VALIDREPRESENTATION",
+	}
+}
+
+func validateCurrencyTestData(currency models.Currency) ValidateCurrencyTestData {
+	return ValidateCurrencyTestData{
+		sut: validatemodel.ValidateCurrency{
+			Currency:      currency,
+			UUIDGenerator: UUIDGeneratorStub{},
+		},
+	}
+}
+
+func TestValidateCurrencyShouldFailOnEmptyName(t *testing.T) {
+	currency := newValidCurrency()
+	currency.Name = ""
+
+	testData := validateCurrencyTestData(currency)
+
+	if testData.sut.Validate() {
+		t.Errorf("Currency considered valid with empty name")
+	}
+}
