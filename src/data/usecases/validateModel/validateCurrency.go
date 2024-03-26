fd91bd3a19a5667395eb05f09c051bb318da6eae
PauloUbirajara <ubirajara3@hotmail.com>
2024-03-26 00:01:32 -0300

    feature: ensure ValidateCategory fails on invalid name and representation

--- src/data/usecases/validateModel/validateCurrency.go
+++ src/data/usecases/validateModel/validateCurrency.go
@@ -19,13 +19,13 @@ func (v ValidateCurrency) Validate() bool {
 	}
 
 	// Name
-	nameRegex, _ := regexp.Compile(".{1,64}")
+	nameRegex, _ := regexp.Compile(`^\S[\S, ]{1,62}\S$`)
 	if !nameRegex.MatchString(v.Currency.Name) {
 		return false
 	}
 
 	// Representation
-	representationRegex, _ := regexp.Compile("[A-Z]{1,5}")
+	representationRegex, _ := regexp.Compile("^[A-Z]{1,5}$")
 	if !representationRegex.MatchString(v.Currency.Representation) {
 		return false
 	}
