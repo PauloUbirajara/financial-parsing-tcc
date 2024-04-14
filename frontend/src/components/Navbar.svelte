630b0d5a6e3955fbb5560831ac3b63649ef400a9
PauloUbirajara <ubirajara3@hotmail.com>
2024-04-13 21:50:46 -0300

    feature: add separate navbar component
    chore: setup path aliases in vite.config.ts

---
+++ frontend/src/components/Navbar.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import {
+    Navbar,
+    NavBrand,
+    NavHamburger,
+    NavUl,
+    NavLi,
+  } from "flowbite-svelte";
+
+  export let isLogged: boolean;
+</script>
+
+<Navbar>
+  <NavBrand href="/">
+    <img src="logo.png" class="me-3 h-6 sm:h-9" alt="Financial Parsing Logo" />
+    <span
+      class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+      >Financial Parsing</span
+    >
+  </NavBrand>
+  <NavHamburger />
+  <NavUl>
+    {#if isLogged}
+      <NavLi href="/dashboard">Dashboard</NavLi>
+      <NavLi href="/transactions">Transações</NavLi>
+      <NavLi href="/wallets">Carteiras</NavLi>
+      <NavLi href="/categories">Categorias</NavLi>
+      <NavLi href="/currencies">Moedas</NavLi>
+    {:else}
+      <NavLi href="/auth/login">Acessar conta</NavLi>
+      <NavLi href="/auth/register">Criar conta</NavLi>
+    {/if}
+  </NavUl>
+</Navbar>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Navbar from "$components/Navbar.svelte";
+</script>
+
+<!-- Navbar -->
+<div class="p-8">
+  <Navbar />
+</div>
+
+<!-- First Content -->
+<!-- Second Content -->
+<!-- Third Content -->
+<!-- Join Content -->
+<!-- Footer -->
