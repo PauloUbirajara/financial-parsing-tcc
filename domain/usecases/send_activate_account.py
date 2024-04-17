df131387388a55d4f051851f564a9df51783fdf4
PauloUbirajara <ubirajara3@hotmail.com>
2024-04-17 17:55:23 -0300

    chore: remove backend files from root folder

--- domain/usecases/send_activate_account.py
+++
@@ -1,5 +0,0 @@
-from abc import ABC
-
-
-class SendActivateAccount(ABC):
-    def send(self): ...
--- protocols/send_email.py
+++
@@ -1,5 +0,0 @@
-from abc import ABC
-
-
-class SendEmail(ABC):
-    def send(self, subject: str, message: str): ...
