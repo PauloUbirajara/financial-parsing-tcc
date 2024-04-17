5ab9b3bf38d58c0c6e81dfc95d9fa82f69640073
PauloUbirajara <ubirajara3@hotmail.com>
2024-04-17 16:30:26 -0300

    feature: create migration to create default currencies

--- backend/apps/currency/models.py
+++ backend/apps/currency/models.py
@@ -6,8 +6,6 @@ from django.db import models
 class Currency(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    user = models.ForeignKey(to="auth.User", on_delete=models.CASCADE)
-
     name = models.TextField(unique=True)
     representation = models.TextField(unique=True)
 
