18eb513270520fccc92c12936af290c83200413d
PauloUbirajara <ubirajara3@hotmail.com>
2024-04-18 16:35:22 -0300

    fix: move authorization check to layout.server

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,11 +1,26 @@
+import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
+import { constants } from "http2";
+import AuthManager from "$lib/auth/AuthManager";
 
 export const load: LayoutServerLoad = async (event) => {
-  const access = event.cookies.get("accessToken");
-  const refresh = event.cookies.get("refreshToken");
+  let isAuthorized = false;
 
-  const isLogged = access !== undefined && refresh !== undefined;
-  return {
-    isLogged,
-  };
+  const access = event.cookies.get("accessToken");
+  if (access !== undefined) {
+    isAuthorized = await AuthManager.validate(access);
+  }
+
+  if (!isAuthorized) {
+    event.cookies.delete("accessToken", { path: "/" });
+    event.cookies.delete("refreshToken", { path: "/" });
+
+    if (event.url.pathname.startsWith("/api")) {
+      redirect(constants.HTTP_STATUS_TEMPORARY_REDIRECT, "/auth/login");
+    }
+  }
+
+  if (isAuthorized && event.url.pathname.startsWith("/auth")) {
+    redirect(constants.HTTP_STATUS_TEMPORARY_REDIRECT, "/api/dashboard");
+  }
 };
