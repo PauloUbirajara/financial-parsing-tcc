89c0dbb4318eeb728236fdcc47b4de625b96c580
PauloUbirajara <ubirajara3@hotmail.com>
2024-04-28 18:48:53 -0300

    fix: change logic to show toasts until certain amount

--- src/lib/toast.ts
+++ src/lib/toast.ts
@@ -4,7 +4,8 @@ import { toastStore } from "../stores/toast";
 const MAX_TOASTS_SHOWN = 3;
 
 export function showToast(message: ToastMessage) {
-  toastStore.update((toasts) =>
-    [message, ...toasts].slice(0, MAX_TOASTS_SHOWN),
-  );
+  toastStore.update((toasts: ToastMessage[]) => [
+    message,
+    ...toasts.slice(0, MAX_TOASTS_SHOWN),
+  ]);
 }
